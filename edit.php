9037eb8ddb1398dfa50ed4808d376206b9f07d45
muinphero <muinphero@gmail.com>
2026-01-11 13:20:21 +0600

    Student Edit header changes

--- edit.php
+++ edit.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <header class="py-10">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-white">
-                    Student Create
+                    Student Edit
                 </h1>
             </div>
         </header>
